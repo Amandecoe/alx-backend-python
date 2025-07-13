63ab617853aa165bb240f2b22e2367870e12f323
Amanuel <amanuelbehailu557@gmail.com>
2025-07-13 15:21:28 +0300

    Boo

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,2 +1,7 @@
-def stream_users_in_batches(batch_size)
-def batch_processing(batch_size)
+def stream_users_in_batches(batch_size):
+  FROM user_data
+  SELECT
+def batch_processing(batch_size):
+  >25
+yield
+return
