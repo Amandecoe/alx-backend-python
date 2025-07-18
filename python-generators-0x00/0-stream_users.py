d68bd80f8c3931207ea65b0530bd36a4efeac86f
Amanuel <amanuelbehailu557@gmail.com>
2025-07-18 12:00:03 +0300

    'streamusers'

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,22 @@
-def stream_users()
+
+import mysql.connector
+
+def stream_users():
+  try:
+    connection = mysql.connector.connect(  #connect() is a function which creates connection based on properties provided and it belongs to mysql.connector
+      host = "localhost",
+      user = "root",                 #sets the properties for the connection
+      password = "",
+      database = "alx_prodev"
+    )
+    cursor= connection.cursor(dictionary = True)
+    cursor.execute("SELECT * FROM user_data")
+   
+    for row in cursor:
+     yield row
+  except Exception as b:
+    print("Wrong connection")
+
+if __name__ == "__main__":
+ for user in stream_users():
+   print(user)
