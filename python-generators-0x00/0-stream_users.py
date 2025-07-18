01fe8088a1509174f8854c16baefad70a37780f1
Amanuel <amanuelbehailu557@gmail.com>
2025-07-18 12:07:05 +0300

    'comment'

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,22 +1,22 @@
 
-import mysql.connector
+import mysql.connector #imports the mysql connector module to enable database operations
 
 def stream_users():
   try:
-    connection = mysql.connector.connect(  #connect() is a function which creates connection based on properties provided and it belongs to mysql.connector
+    connection = mysql.connector.connect(  #establishes a connection to a database specified below
       host = "localhost",
       user = "root",                 #sets the properties for the connection
       password = "",
       database = "alx_prodev"
     )
-    cursor= connection.cursor(dictionary = True)
-    cursor.execute("SELECT * FROM user_data")
+    cursor= connection.cursor(dictionary = True)  #cursor is a pointer to a database or points to a database, the use of dictionary here makes each row return as a column:value
+    cursor.execute("SELECT * FROM user_data") #is what is used to run an sql query. fetches every row here
    
     for row in cursor:
-     yield row
+     yield row     #loops through each row in the query result and instead of returing all at once it yields one row at a time
   except Exception as b:
     print("Wrong connection")
 
 if __name__ == "__main__":
  for user in stream_users():
-   print(user)
+   print(user)    #iterators over the above generator fetching each row as a dictionary as provided in the function
