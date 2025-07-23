7f4707e7423923f54eaec478138d6decd68b1abf
Amanuel <amanuelbehailu557@gmail.com>
2025-07-23 13:17:38 +0300

    'loggingquery'

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,10 +1,33 @@
 import sqlite3
+import time
 
 def log_queries(func):
-  def wrapper():
-    
+  def wrapper(*args , **kwargs):
+      start = time.time()
+      result = func(*args, **kwargs)  # Run the actual query
+      duration = time.time() - start
+        
+        # Get the SQL query (assuming it's the first argument)
+      sql = args[0] if args else "No query captured"
+      print(f"Query took {duration:.4f} seconds: {sql[:50]}...")  # Show first 50 chars
+        
+      return result
+  return wrapper  
 
-
+def populate_table(conn, name, email, age):
+   conn = sqlite3.connect('usersin.db')
+   cursor = conn.cursor()
+   try:
+      cursor.execute(
+         'INSERT INTO TABLE user(name, email, age)'
+         'VALUES(?,?,?)', (name, email, age)
+      )
+      conn.commit()
+      print ("data added successfully")
+   except sqlite3.Error as k:
+      print ("Can not enter data into table")
+   finally:
+      conn.close()   
 
 def create_table():
    conn = sqlite3.connect('usersin.db')
@@ -16,18 +39,27 @@ def create_table():
                     email TEXT UNIQUE NOT NULL,
                     age INTEGER)''')
      print("Table created successfuly")
+     conn.commit()
    except sqlite3.Error as b:
      print("Can not create table")
+   finally:
+      conn.close()
 
 
+@log_queries
 def fetch_user_data(query):
   conn = sqlite3.connect('usersin.db')
   cursor = conn.cursor()
-  cursor.execute()
+  cursor.execute(query)
   results = cursor.fetchall()
   conn.close()
   return results
 
 
+def main():
+   conn = sqlite3.connect('usersin.db')
+   create_table()
+   populate_table(conn,"Aman", "aman123@gmail.com",20)
+if __name__ == "__main__":
+    main()   
 
-users = fetch_user_data(query = "SELECT * FROM users")
