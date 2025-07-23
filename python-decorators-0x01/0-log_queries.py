ad494dbc59e37842011752e06fecc5b571688130
Amanuel <amanuelbehailu557@gmail.com>
2025-07-23 12:45:26 +0300

    'Newtableanddecoratorstart'

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,60 +1,33 @@
 import sqlite3
 
-class Database:
-    def __init__(self, db_name):
-        self.conn = sqlite3.connect(f"{db_name}.db")  # Added .db extension
-        self.cursor = self.conn.cursor()  # Fixed cursor creation
+def log_queries(func):
+  def wrapper():
+    
 
-    def create_table(self):
-        try: 
-            self.cursor.execute('''
-                CREATE TABLE IF NOT EXISTS users( 
+
+
+def create_table():
+   conn = sqlite3.connect('usersin.db')
+   cursor = conn.cursor()
+   try:
+     cursor.execute('''CREATE TABLE IF NOT EXISTS user(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT NOT NULL,
                     email TEXT UNIQUE NOT NULL,
-                    age INTEGER) 
-            ''')
-            self.conn.commit()
-            print("Table created successfully")
-        except sqlite3.Error as e:
-            print(f"Table not created: {e}")  # Added error message
-
-    def table_population(self, name, email, age):
-        try:
-            self.cursor.execute(
-                "INSERT INTO users (name, email, age) VALUES (?, ?, ?)",
-                (name, email, age)
-            )
-            self.conn.commit()
-            print("Data added successfully")
-        except sqlite3.Error as e:
-            print(f"Cannot add to table: {e}")  # Added error message
-    def retrieve_data(self):
-        try:
-            self.cursor.execute(
-            "SELECT * FROM users"
-           )
-            rows = self.cursor.fetchall()
-            if not rows:
-                print("no data found in table")
-                return []
-        
-            print("data retrieved successfully")
-            for row in rows:
-                print(row)
-                return rows  
-            
-        except sqlite3.Error as J:
-            print("Can not retrieve data")
-            
-def main():
-    db = Database("db_name")  # Will create users.db
-    db.retrieve_data()
-    db.conn.close()  # Important to close connection
-
-if __name__ == "__main__":
-    main()
-
- 
+                    age INTEGER)''')
+     print("Table created successfuly")
+   except sqlite3.Error as b:
+     print("Can not create table")
 
 
+def fetch_user_data(query):
+  conn = sqlite3.connect('usersin.db')
+  cursor = conn.cursor()
+  cursor.execute()
+  results = cursor.fetchall()
+  conn.close()
+  return results
+
+
+
+users = fetch_user_data(query = "SELECT * FROM users")
