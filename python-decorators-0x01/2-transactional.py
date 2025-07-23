4331c3aab65b2ae942974ee45d7c0c95d58aa2a4
Amanuel <amanuelbehailu557@gmail.com>
2025-07-23 17:30:43 +0300

    'justanupdate'

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -1,41 +1,44 @@
-import sqlite3
+import sqlite3 
 import functools
 
 def with_db_connection(func):
-    """Decorator to handle database connection"""
     @functools.wraps(func)
-    def wrapper(*args, db_name='users.db', **kwargs):
-        conn = sqlite3.connect(db_name)
+    def wrapper(*args, **kwargs):
+        conn = None
         try:
-            return func(conn, *args, **kwargs)
+            conn = sqlite3.connect('usersin.db')
+            print("Database connection established")
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except sqlite3.Error as e:
+            print("Cannot establish database connection:", e)
+            return None
         finally:
-            conn.close()
+            if conn:
+                conn.close()
+                print("Database closed successfully")
     return wrapper
+
 
 def transactional(func):
-    """Decorator to handle database transactions"""
-    @functools.wraps(func)
-    def wrapper(conn, *args, **kwargs):
-        try:
-            result = func(conn, *args, **kwargs)
-            conn.commit()  # Commit if no exceptions
-            return result
-        except Exception as e:
-            conn.rollback()  # Rollback on error
-            raise e  # Re-raise the exception
-    return wrapper
+    def wrapper():
 
-@with_db_connection
-@transactional
-def update_user_email(conn, user_id, new_email):
-    cursor = conn.cursor()
-    cursor.execute(
-        "UPDATE users SET email = ? WHERE id = ?", 
-        (new_email, user_id)
-    )
+     return wrapper    
+ 
+ 
 
-# Usage
-update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+ cursor = conn.connect()
+ cursor = conn.cursor() 
+ cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+#### Update user's email with automatic transaction handling 
 
 
-##code doesn't work!!!
+def main():
+ update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
+
+if __name__=="__main__":
+    main() 
