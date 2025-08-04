1b9f8172db5f128eef2b8767b2d60309fd81cd09
Amanuel <amanuelbehailu557@gmail.com>
2025-08-04 12:05:08 +0300

    'contextmanagers'

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,20 +1,27 @@
 import sqlite3
 
 class DatabaseConnection:
-  def __enter__(conn,query):
+  def __enter__(self):  #is used to open the connection
    try:
-    conn = sqlite3.connect('usersin.db')
-    cur = conn.cursor()
-    cur.execute(query)
-    conn.commit() 
+    self.conn = sqlite3.connect('usersin.db')
+    print ("Database opened succesfully")
    except sqlite3.Error as b:
     print("Can not open database")
-  def __exit__(conn):
+  def __exit__(self,exc_type, exc_val, exc_tb): # is used to close the connection
    try:
     conn = sqlite3.connect('usersin.db')
+    print ("database closed")
    except sqlite3.Error as c:
     print ("Can not close connection")
    finally:
     conn.close() 
 try:
+ with sqlite3.connect('usersin.db') as conn:
+  cursor=conn.cursor()
+  cursor.execute('SELECT * FROM users')
+  result = cursor.fetchall()
+except sqlite3.Error as c:
+ print ("Error executing query")   
+
+
      
