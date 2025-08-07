f3c0706fa20ed7a92d9f78b78ad5410922517b4e
Amanuel <amanuelbehailu557@gmail.com>
2025-08-07 16:35:26 +0300

    dssd

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,7 @@ class TestAccessNestedMap(unittest.TestCase):
   ({"a": {"b": 2}}, ("a",), {"b": 2}),
   ({"a":{"b": 2}}, ("a", "b"), 2),
    ])
-  def test_Access_Nested_Map(self, nested_map, path, expected ):
+  def test_access_nested_map(self, nested_map, path, expected ):
     self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
