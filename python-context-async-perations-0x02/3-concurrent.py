a7a96377c6d530852d7698a5db25f257a9d0e531
Amanuel <amanuelbehailu557@gmail.com>
2025-08-10 17:47:20 +0300

    base

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -2,10 +2,19 @@ import asyncio
 import aiosqlite
 import sqlite3
 
-async def _fetch_users(): 
- conn = sqlite3.connect('userindb')
- cursor = cursor.conn()
- cursor.execute('SELECT * USERS from users')
+async def _fetch_users(conn): 
+ try:
+  conn = sqlite3.connect('usersin.db')
+  cursor = cursor.conn()
+  await cursor.execute('SELECT * USERS from users')
+ except sqlite3.Error as d:
+  print("Can not connect to database")
 
+async def _fetch_older_users(conn):
+ try:
+  conn = sqlite3.connect('usersin.db')
+  cursor = cursor.conn()
+  await cursor.execute ('SELECT USERS WHERE Age > 40')
+ except sqlite3.Error as x:
+  print ("Can not connect to database")
 
-async def 
