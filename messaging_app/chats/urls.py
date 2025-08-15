33ea5697663a491d6e37b07fbd3525932c8bccf7
Amanuel <amanuelbehailu557@gmail.com>
2025-08-15 22:42:15 +0300

    urls.py updated

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,4 +1,4 @@
-from rest_framework import routers.DefaultRouter()
+from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 
@@ -6,5 +6,5 @@ routers = DefaultRouter()
 #Automatically creates a RESTful URLs for the viewset
 routers.register(r'conversations', ConversationViewSet)
 #Registers the conversationview at /conversations/
-urlpatterns = routers.urls
+urlpatterns = [path (' ', include(router.irls))] 
 #connects all these URLS to Django's routing system
