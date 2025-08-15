390839dc02a676de7bb0e19df05ea08d13d9aa89
Amanuel <amanuelbehailu557@gmail.com>
2025-08-15 19:47:34 +0300

    Serialization final

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -15,6 +15,12 @@ class UserSerializer(serializers.ModelSerializer):
       'password_hash',
       'role'
     )
+  def validate_first_name(self, value):
+    if value <=0:
+       raise serializers.ValidationError(
+         "No first name"
+      )
+    return value
     
 class MessageSerializer(serializers.ModelSerializer):
   class Meta:
@@ -28,10 +34,18 @@ class MessageSerializer(serializers.ModelSerializer):
 
 class ConversationSerializer(serializers.ModelSerializer):
   Message = serializers.CharField(read_only=True)
+  last_message = serializers.SerializerMethodField()
+  #is a computed field which is not stored in the database
+  #this calls a function named get_last_message when serializing a conversation
   class Meta:
     model = Conversation
     fields = (
-      'conversation_id'
-      'participants_id'
-      'created_at'
+      'conversation_id',
+      'participants_id',
+      'created_at',
     )
+  def get_last_message(self, obj): #obj is the conversation instance being serialized
+        last_msg = obj.messages.order_by('-sent_at').first()  # gets all related messages ordered by sent time
+        if last_msg:
+            return MessageSerializer(last_msg).data #serializes the message instance into JSON, .data gives the dictionary representation
+        return None  
