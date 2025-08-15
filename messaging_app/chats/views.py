6faa8ed2270524ca76033193fdebc2971ff148ba
Amanuel <amanuelbehailu557@gmail.com>
2025-08-15 20:14:09 +0300

    views sample

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 # Create your views here.
-class ConversationViewSet(generics.ListCreateAPIView):
+class ConversationViewSet(viewsets.ModelViewSet):
+  queryset = Conversation.object.all()
+  #where query is run from 
+  serializer_class = ConversationSerializer
+  #serializer we use when returning this data
+  
