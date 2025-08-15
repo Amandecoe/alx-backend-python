e58a9de8fd0dfe3e7630597b9eda162e6643ee5d
Amanuel <amanuelbehailu557@gmail.com>
2025-08-15 19:27:42 +0300

    sample for serializing nested relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -16,3 +16,22 @@ class UserSerializer(serializers.ModelSerializer):
       'role'
     )
     
+class MessageSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Message
+    fields = (
+      'message_id',
+      'sender_id',
+      'message_body',
+      'sent_at',
+    )
+
+class ConversationSerializer(serializers.ModelSerializer):
+  Message = serializers.CharField(read_only=True)
+  class Meta:
+    model = Conversation
+    fields = (
+      'conversation_id'
+      'participants_id'
+      'created_at'
+    )
