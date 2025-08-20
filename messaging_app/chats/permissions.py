137c6bcd0a356e0b033da6f2565c6bdadba8e098
Amanuel <amanuelbehailu557@gmail.com>
2025-08-20 10:43:44 +0300

    Fall

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -7,4 +7,7 @@ class IsParticipantOfConversation(BasePermission):
   def has_object_permission(self, request, view, obj):
         # obj will be a Message instance
         conversation = obj.conversation
-        return request.user in conversation.participants.all()
+        if not request.user in conversation.participants.all():
+             return False
+        if not request.method in ["PUT", "PATCH", "DELETE", "GET", "POST"]:
+             return False
