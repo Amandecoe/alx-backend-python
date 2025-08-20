bae48d4bc62538a52b24066d8be2e2c581e3f648
Amanuel <amanuelbehailu557@gmail.com>
2025-08-20 10:24:13 +0300

    ssa

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,4 +3,4 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsParticipantOfConversation:
   def has_permission(self, request, view):
-    return bool(request.user and request.user.isAuthenticated)
+    return bool(request.user and request.user.is_Authenticated)
