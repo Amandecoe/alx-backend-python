e585dde3a15049c66496ea6ba4fe1296d76fa5b5
Amanuel <amanuelbehailu557@gmail.com>
2025-08-21 10:20:15 +0300

    successfully run the server

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,7 +1,10 @@
-from rest_framework import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination
 import math
-class MessageFilter(PageNumberPagination):
-  page= 20            # number of messages per page
-  page_size_query_param = 'page_size'  # optional, let client override
-  max_page_size = 100       # prevent abuse
-  page = page.paginator.count(20)
+class MessageFilter( PageNumberPagination):
+  page= 20 # number of messages per page
+  def paginated_response(self):           
+      page_size_query_param = 'page_size'  # optional, let client override
+      max_page_size = 100       # prevent abuse
+      total_items = self.page.paginator.count    # total messages in queryset
+      current_page = self.page.number            # current page number
+      page_size = self.get_page_size(self.request)
