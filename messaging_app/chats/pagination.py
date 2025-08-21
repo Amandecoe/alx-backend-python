2fa3dd3880970092265ac2a3339359dfd0a06f5f
Amanuel <amanuelbehailu557@gmail.com>
2025-08-21 09:55:37 +0300

    page.paginator.count added

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,7 @@
 from rest_framework import PageNumberPagination
-
+import math
 class MessageFilter(PageNumberPagination):
-  page_size = 20            # number of messages per page
+  page= 20            # number of messages per page
   page_size_query_param = 'page_size'  # optional, let client override
   max_page_size = 100       # prevent abuse
+  page = page.paginator.count(20)
