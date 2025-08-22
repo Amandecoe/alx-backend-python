df240c16c97a1c58b0b1de32e566075964d2dc6b
Amanuel <amanuelbehailu557@gmail.com>
2025-08-22 11:41:00 +0300

    Request.log

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,22 +1,17 @@
 from django.conf import Settings
 from django.core.exceptions import PermissionDenied
-from time import datetime
+from datetime import datetime
 import logging
 
 class RequestLoggingMiddleware:
-  def _init_(self, get_response):
-   self.get_response = get_response
+  def __init__(self, get_response):
+      self.get_response = get_response
   
   def __call__(self, request):
-    user = 'Anonymous'
-    if hasattr(request, 'user') and hasattr(request.user, 'is_Authenticated'):
-      if request.user.is_Authenticated:  
-        user = request.user.username
+      user = request.user if request.user.is_authenticated else "Anonymous"
+      with open ("requests.log", "a") as f:
+        f.write(f"{datetime.now()} - User: {user} - Path: {request.path}\n")
 
-    log_message = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
-    with open ('requests.log', 'a') as f:
-      f.write(log_message)
-
-    response = self.get_response(request)
-    return response  
+      response = self.get_response(request)
+      return response  
 
