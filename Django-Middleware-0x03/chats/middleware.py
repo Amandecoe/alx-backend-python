4c4a0efdd035d991cd2dc6b8084ae73274618a52
Amanuel <amanuelbehailu557@gmail.com>
2025-08-22 12:05:19 +0300

    Middleware time checker

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import logging
 
 class RequestLoggingMiddleware:
-  def __init__(self, get_response):
+  def __init__(self, get_response): #is the function that accepts the response everytime 
       self.get_response = get_response
   
   def __call__(self, request):
@@ -15,3 +15,13 @@ class RequestLoggingMiddleware:
       response = self.get_response(request)
       return response  
 
+
+class  RestrictAccessByTimeMiddleware:
+   def __init__(self, get_response):
+      self.get_response = get_response
+   def __call__(self,request):
+      user = request.user if request.user.is_authenticated else "Anonymous"
+      current_time = datetime.now().time()
+      if (18,0)< current_time <(21,0) :
+       raise PermissionDenied 
+         
