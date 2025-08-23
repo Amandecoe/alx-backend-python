9d245e73c0f544f9dc59ec1c6fbc62b49a2f0f68
Amanuel <amanuelbehailu557@gmail.com>
2025-08-23 17:48:10 +0300

    Offensive Langugae Middleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -2,7 +2,9 @@ from django.conf import Settings
 from django.core.exceptions import PermissionDenied
 from datetime import datetime
 import logging
-
+import time
+from collections import deque
+from django.http import JsonResponse
 class RequestLoggingMiddleware:
   def __init__(self, get_response): #is the function that accepts the response everytime 
       self.get_response = get_response
@@ -20,8 +22,30 @@ class RestrictAccessByTimeMiddleware:
    def __init__(self, get_response):
       self.get_response = get_response
    def __call__(self,request):
-      user = request.user if request.user.is_authenticated else "Anonymous"
       current_time = datetime.now().time()
       if (18,0)< current_time <(21,0) :
        raise PermissionDenied 
          
+#class to limit users to send only 5 messages per 60 seconds
+class OffensiveLanguageMiddleware:
+  def __init__(self, get_response):
+     self.get_response =  get_response
+     self.request_log = {}
+     self.time_window = 60
+     self.max_requests = 5
+  def __call__(self,request):
+     client_ip = self.get.client_ip(request) #gets the clients Ip and stores it
+     now = time.time()
+
+     if client_ip not in self.request_log:  #checks if the client's ip is in the logged ip's in our request.log file
+        self.request_log[client_ip] = deque() #then it gives a client_ip an empty bucket to store the timestamp of that request
+
+     request_times = self.request_log[client_ip] #stores the amount of times a user have requested using the client's ip from the request_log 
+
+     if len(request_times) > self.max_requests:  #checks the number of requests and if it passes the max requests allowed it puts out an error message
+        return JsonResponse(
+           {"Error":"Limit Reached Try Again Later"},
+           status = 429
+        )
+     
+     
