144fa822797a02d46c0f4d82e9acc67a90d16adf
Amanuel <amanuelbehailu557@gmail.com>
2025-08-24 10:47:44 +0300

    Models defined

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class sender(models.Model):
+  sender_id = models.PositiveIntegerField(primary_key=True)
+  sender_name = models.CharField(max_length=100)
+  email = models.EmailField(unique= True, null= False, blank= False)
+
+class receiver(models.Model):
+  receiver_id = models.PositiveIntegerField(primary_key = True)
+  receiver_name = models.CharField(max_length=100)
+  email = models.EmailField(unique = True, null = False, blank = False)
+  senders_id = models.ForeignKey(sender, on_delete = models.CASCADE)
+  
+class content(models.Model):
+  content_id = models.PositiveIntegerField(primary_key = True)
+  content_body = models.TextField(max_length = 250, null = False)
+  sent_at = models.DateTimeField(auto_now_add = True)
+
+class timestamp(models.Model):
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
+
+  
