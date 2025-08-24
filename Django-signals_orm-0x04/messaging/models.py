cf022ea013b1d9e6eb1002c7a24cb5d9e27985c6
Amanuel <amanuelbehailu557@gmail.com>
2025-08-24 13:15:27 +0300

    Message history viewer defined

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -40,3 +40,4 @@ class MessageHistory(models.Model):
   message = models.ForeignKey(Message, on_delete = models.CASCADE, related_name = "history")
   content = models.TextField()
   edited_at = models.DateTimeField()
+  edited_by = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
+from .models import Message
 
 # Create your views here.
+def message_history_view(request, message_id):
+  message = get_object_or_404(Message, id = message_id)
+  #gets the specific message by its id, if it doesn't exist it returns 404 page
+  history = message.history.order_by('_edited_at')
+  #fetches all associated history of that message
+  context = {
+    'message': message,
+    'history' : history,
+  }
+  #the specific message and its history are passed to this list context
+  return render(request, context)
+  #renders the template with context
