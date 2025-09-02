4b01f127be0cff78721dbd6bc6b612a4428b4ba7
Amanuel <amanuelbehailu557@gmail.com>
2025-09-02 07:03:29 +0300

    A

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -3,6 +3,9 @@ from .models import Message
 from django.views.decorators.cache import cache_page
 from django.contrib.auth import get_user_model
 from .managers import unread_for_user
+#create 
+User = get_user_model()
+# Create your views here. 
 @cache_page(60 * 5)
 def message_history_view(request, message_id):
   message = get_object_or_404(Message, id = message_id)
