d4b689baeae6bace2bc075db59e035cc80a1524c
Amanuel <amanuelbehailu557@gmail.com>
2025-11-27 22:35:26 +0300

    html tempelate

--- Weatherproject/Weather/Weatherapp/views.py
+++ Weatherproject/Weather/Weatherapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, HttpResponse
-
+import requests
+import datetime
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome to the Django Server')
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'indoor'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=dd2f8a056a0e3b707b8b7838153aba19'
+
+    PARAMS = {'units':'metric'}
+    data = request.get(url, PARAMS).json()
+
+    description = data ['weather'][0]['description'] #description of the weather
+    icon = data ['weather'][0]['icon'] #description of the weather icon
+    temp = data['main']['temp'] #temperature of our city
+
+    day = datetime.date.today()
+
+    return render(request, 'Weatherapp/index.html',{'descritption':description, 'icon':icon, 'temp':temp, 'day':day})
